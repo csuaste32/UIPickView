dada379799a500ec218920ca4ac8d1dc49f6ed81
Cynthia Suaste <cyndysuaste@gmail.com>
2019-07-17 12:06:26 -0500

    x

--- UIPickView/ViewController.swift
+++ UIPickView/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    let student = ["Jose", "Cynthia", "Aidan", ]
+    
+    @IBOutlet var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { return 1
     }
-
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return student.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return student[row]
+    }
 }
 
